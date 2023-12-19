de7e401a6d3155816a1a4d99ba67d6168d58bbb0
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-19 08:32:30 -0300

    add

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -14,3 +14,19 @@ int inteiro = 32;
 string teste = inteiro.ToString() + 10;
 
 Console.WriteLine(teste);
+
+int n1 = 6789;
+int n2 = 193;
+double n3 = 333.0;
+double n4 = 1234.55;
+
+double geralContas = n4 / (n1 * (n2 + n3));
+
+Console.WriteLine(geralContas);
+
+string valorA = "13!";
+int valorB = 0;
+
+int.TryParse(valorA, out valorB);
+
+Console.WriteLine(valorB);
