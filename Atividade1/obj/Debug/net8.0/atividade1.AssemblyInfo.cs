78bba4052a1ece4857e7f9a0cbb46175505984bb
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-19 10:43:40 -0300

    add

--- Atividade1/obj/Debug/net8.0/atividade1.AssemblyInfo.cs
+++ Atividade1/obj/Debug/net8.0/atividade1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7e401a6d3155816a1a4d99ba67d6168d58bbb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b976096d3c92c9eb866b4c93c1e6b483460ec02")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
