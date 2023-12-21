0cf63905839187a5f051da5dfda56b60a8d5d1f6
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-21 12:15:14 -0300

    add

---
+++ Atividade4/Program.cs
@@ -0,0 +1,8 @@
+﻿using Atividade4.Models;
+
+Calculadora novaConta = new Calculadora();
+novaConta.Somar(10, 25);
+novaConta.Subtrair(10, 25);
+novaConta.Multiplicar(10, 20);
+novaConta.Dividir(10, 25);
+novaConta.Potencia(10, 3);
