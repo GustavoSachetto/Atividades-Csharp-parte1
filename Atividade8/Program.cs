bc9c9e67d862bf249f57ea807f788628a4763570
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-26 14:02:16 -0300

    add

---
+++ Atividade8/Program.cs
@@ -0,0 +1,36 @@
+﻿int media = 0;
+int[] arrayInteiros = new int[3]; // Declarando o array em C#
+
+Console.WriteLine("Digite um numero");
+arrayInteiros[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero");
+arrayInteiros[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite mais outro numero");
+arrayInteiros[2] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("==========================");
+Console.WriteLine("Numeros digitados (For):");
+
+// Varrendo array com for
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    media += arrayInteiros[i];
+    Console.WriteLine($"Posição N°{i} - {arrayInteiros[i]}");
+}
+
+Console.WriteLine($"Media final: {media/3}");
+Console.WriteLine("==========================");
+
+Console.WriteLine("Numeros digitados (Foreach):");
+media = 0;
+
+// Varrendo array com foreach
+foreach (var item in arrayInteiros)
+{
+    media += item;
+    Console.WriteLine($"Elemnto no array: - {item}");
+}
+
+Console.WriteLine($"Media final: {media/3}");
--- Atividade8/obj/Debug/net8.0/atividade8.AssemblyInfo.cs
+++ Atividade8/obj/Debug/net8.0/atividade8.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("atividade1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("atividade8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("atividade1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("atividade1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8bbf7025511d7c0fbc48d63e0d44e260fe7b9f")]
+[assembly: System.Reflection.AssemblyProductAttribute("atividade8")]
+[assembly: System.Reflection.AssemblyTitleAttribute("atividade8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
